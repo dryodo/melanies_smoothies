259265e1cf4e8d1d0e52d0cd9380dc2cc7932761
dryodo <yodope@hotmail.com>
2025-12-25 16:53:37 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-import pandas as pd
+import pandas
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
